412878e9c5d6f525b332978735ff0930d89bcc19
Wietse van Ginkel <info@ginkelsoft.com>
2025-04-10 14:49:50 +0200

    Update GlobalSearch

--- src/View/Components/BuildoraGlobalSearch.php
+++ src/View/Components/BuildoraGlobalSearch.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class GlobalSearch extends Component
+class BuildoraGlobalSearch extends Component
 {
     /**
      * Render the component view.
