e6ef9fab8d6052e0de82d541255f7d41b2605ec9
Wietse van Ginkel <info@ginkelsoft.com>
2025-04-16 08:49:44 +0200

    Added Dashboard Widgets only
    Fixes some style issues

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,12 +1,5 @@
 <nav class="text-sm font-medium text-muted-foreground">
     <ul class="flex items-center space-x-2">
-        <li>
-            <a href="{{ route('buildora.dashboard') }}" class="hover:text-foreground transition-colors">
-                <x-buildora-icon icon="fa fa-home" class="text-muted-foreground" />
-                {{ __buildora('Dashboard') }}
-            </a>
-        </li>
-
         @foreach($buildoraBreadcrumbs as $index => $item)
             <li class="flex items-center">
                 <x-buildora-icon icon="fa fa-chevron-right" class="text-muted-foreground text-xs mx-2" />
