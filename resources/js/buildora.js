35b1ca8556cd91525172f8e4a2c1eeb9fa660bca
GinkelSoft <info@ginkelsoft.com>
2025-06-10 13:51:54 +0200

    Add dynamic repeatable field naming

--- resources/js/buildora.js
+++ resources/js/buildora.js
@@ -6,3 +6,23 @@ window.Alpine = Alpine;
 Alpine.start();
 
 console.log("Alpine.js loaded!");
+
+function updateRepeatableFieldNames(form) {
+    form.querySelectorAll('[data-repeatable-index]').forEach((row) => {
+        const index = row.dataset.repeatableIndex;
+        row.querySelectorAll('[name]').forEach((el) => {
+            if (el.name && el.name.includes('${index}')) {
+                el.name = el.name.replaceAll('${index}', index);
+            }
+        });
+    });
+}
+
+window.updateRepeatableFieldNames = updateRepeatableFieldNames;
+
+document.addEventListener('submit', (e) => {
+    const form = e.target.closest('form');
+    if (form) {
+        updateRepeatableFieldNames(form);
+    }
+}, true);
