4e32e74ee70fa6123a765a0832a80958bda201b4
Wietse van Ginkel <info@ginkelsoft.com>
2025-10-21 10:31:12 +0200

    sdad

--- src/Resources/Defaults/PermissionBuildora.php
+++ src/Resources/Defaults/PermissionBuildora.php
@@ -2,7 +2,9 @@
 
 namespace Ginkelsoft\Buildora\Resources\Defaults;
 
+use Ginkelsoft\Buildora\Fields\Types\TextField;
 use Ginkelsoft\Buildora\Resources\ModelResource;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Permission;
 
 class PermissionBuildora extends ModelResource
@@ -26,6 +28,34 @@ class PermissionBuildora extends ModelResource
     protected function confirmDeleteMessage(): string
     {
         return 'Are you sure you want to delete this permission?';
+    }
+
+    protected function finalizeFields(array $fields, Model $model): array
+    {
+        $labelFieldIndex = null;
+
+        foreach ($fields as $index => $field) {
+            if ($field->name === 'label') {
+                $labelFieldIndex = $index;
+                break;
+            }
+        }
+
+        $labelField = $labelFieldIndex !== null
+            ? $fields[$labelFieldIndex]
+            : TextField::make('label', 'Label');
+
+        $labelField
+            ->validation(['required', 'string', 'max:255'])
+            ->help('Dit label wordt gebruikt om permissies leesbaar te tonen in de interface.');
+
+        if ($labelFieldIndex === null) {
+            $fields[] = $labelField;
+        } else {
+            $fields[$labelFieldIndex] = $labelField;
+        }
+
+        return parent::finalizeFields($fields, $model);
     }
 
     public static function modelClass(): string
