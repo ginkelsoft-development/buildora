bdb7ab3c2eaad5c92e4135d33ed264ad7b2fedaf
Wietse van Ginkel <info@ginkelsoft.com>
2025-10-21 10:11:22 +0200

    sdad

---
+++ src/Resources/Defaults/UserBuildora.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ginkelsoft\Buildora\Resources\Defaults;
+
+use Ginkelsoft\Buildora\Resources\ModelResource;
+use Illuminate\Database\Eloquent\Model;
+
+class UserBuildora extends ModelResource
+{
+    protected array $excludeFields = ['remember_token'];
+
+    public function title(): string
+    {
+        return 'User';
+    }
+
+    public function searchResultConfig(): array
+    {
+        return [
+            'label' => ['name', 'email'],
+            'columns' => ['name', 'email'],
+        ];
+    }
+
+    protected function confirmDeleteMessage(): string
+    {
+        return 'Are you sure you want to delete this user?';
+    }
+
+    protected function additionalFields(Model $model): array
+    {
+        return [];
+    }
+
+    public static function modelClass(): string
+    {
+        $configured = config('auth.providers.users.model');
+
+        if ($configured) {
+            return $configured;
+        }
+
+        return parent::modelClass();
+    }
+}
